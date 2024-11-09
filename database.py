6efa1e6830259f0bcda73572147a4e2a092c48e7
Andrey <127301400+progito@users.noreply.github.com>
2024-11-09 15:05:39 +0400

    Update database.py

--- database.py
+++ database.py
@@ -1,1 +1,11 @@
+from flask import Flask
+from flask_migrate import Migrate
+from models import db
 
+def create_app():
+    app = Flask(__name__)
+    ....
+    
+    migrate = Migrate(app, db)  # Инициализация миграции
+
+    return app
